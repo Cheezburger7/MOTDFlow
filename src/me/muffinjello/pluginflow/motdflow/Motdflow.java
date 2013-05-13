1ba947a8d96f38f666c40bc517882bee281e4c3c
muffinjello <muffinjello@outlook.com>
2013-05-13 11:20:04 -0700

    All the commands and methods are working!
    Excluding the MOTD changing event

--- src/me/muffinjello/pluginflow/motdflow/Motdflow.java
+++ src/me/muffinjello/pluginflow/motdflow/Motdflow.java
@@ -6,46 +6,52 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Motdflow extends JavaPlugin implements Listener {
     public String Motd = "";
     @Override
     public void onEnable() {
+        PluginDescriptionFile pdf = this.getDescription();
         saveDefaultConfig();
-        this.getConfig().getString("motd", Motd);
+        Motd = getConfig().getString("motd", Motd);
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if(cmd.getName().equalsIgnoreCase("motd")){
-            if(args.length < 2){
+        if(cmd.getName().equalsIgnoreCase("motd") && sender.hasPermission("motd.use")){
+            if(args.length < 1){
                 sender.sendMessage(ChatColor.GRAY + "[Flow] Correct usage: ");
                 sender.sendMessage(ChatColor.YELLOW + "/motd set <NEW MOTD>");
                 sender.sendMessage(ChatColor.YELLOW + "/motd reload-config");
                 sender.sendMessage(ChatColor.YELLOW + "/motd view");
             } else {
-                if (args.length >= 2){
-                    if(args[0].equalsIgnoreCase("set")){
+                if (args.length >= 1){
+                    if(args[0].equalsIgnoreCase("set") && args.length > 1){
                         String s = "";
                         for (int x = 1; x < args.length; x++){
                             s = s + args[x] + " ";
                             Motd = s;
-                            this.getConfig().set("motd", Motd);
+                            getConfig().set("motd", Motd);
+                            this.saveConfig();
                         }
-                        sender.sendMessage(s);
+                        sender.sendMessage(ChatColor.GRAY + "[Flow] MOTD set: " + ChatColor.YELLOW + s);
                         return true;
                     }
                     else if(args[0].equalsIgnoreCase("view")){
                         sender.sendMessage(ChatColor.GRAY + "[Flow] The current MOTD is: " + ChatColor.YELLOW + Motd);
+                        return true;
                     }
                     else if(args[0].equalsIgnoreCase("reload-config")){
-                        sender.sendMessage(ChatColor.GRAY + "Reloaded MOTD from the config!");
-                        this.getConfig().getString("motd", Motd);
+                        sender.sendMessage(ChatColor.GRAY + "[Flow] Reloaded MOTD from the config!");
+                        this.reloadConfig();
+                        Motd = this.getConfig().getString("motd", Motd);
+                        return true;
                     }
                 }
             }
         }
-        return true;
+        return false;
     }
 }
 
